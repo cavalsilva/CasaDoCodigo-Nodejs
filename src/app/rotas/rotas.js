63a41137a8cb20c39f19bf46ced3df3f187df38f
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-04 06:24:57 -0300

    Criando template dinamico com marko

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -20,7 +20,19 @@ module.exports = (app) => {
     // Rota Livros "/livros"
     app.get('/livros', function (req, resp) {
         resp.marko(
-            require('../views/livros/lista/lista.marko')
+            require('../views/livros/lista/lista.marko'),
+            {
+                livros: [
+                    { 
+                        id: 1,
+                        titulo: 'Fundamentos do Node'
+                    },
+                    { 
+                        id: 2,
+                        titulo: 'Node Avançado'
+                    }
+                ]
+            }
         );
     });
 }
