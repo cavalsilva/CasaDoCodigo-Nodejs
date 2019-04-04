223eca8ce51a41efb8f1804532373d6654a7542e
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-04-04 06:42:00 -0300

    Acessando o Banco de dados

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,5 @@
+const db = require('../../config/database');
+
 // Exporta uma função
 module.exports = (app) => {
 
@@ -19,21 +21,16 @@ module.exports = (app) => {
 
     // Rota Livros "/livros"
     app.get('/livros', function (req, resp) {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
-                    { 
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    { 
-                        id: 2,
-                        titulo: 'Node Avançado'
-                    }
-                ]
-            }
-        );
+        db.all('SELECT * FROM livros', function (erro, resultados) {
+
+            resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: resultados
+                }
+            );
+
+        });
     });
 }
 
